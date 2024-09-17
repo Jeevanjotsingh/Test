3a299b6ffaa892cfae93b73bfc85b98d0e1749b4
Jeevanjotsingh <jeevanjotsingh9906@gmail.com>
2024-09-17 19:05:15 +0530

    help

---
+++ page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p>Welcome to your ChatGPT Voice Web application.</p>
+    </div>
+  );
+}
